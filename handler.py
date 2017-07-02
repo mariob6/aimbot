51a2f35fab61c32c33928b28d52cb6c281b4d26d
mariob6 <berahamario@gmail.com>
2017-07-02 22:06:41 +0200

    adding insults

--- handler.py
+++ handler.py
@@ -6,6 +6,9 @@ class MessageHandler(object):
 
     _HI = ['ciao', 'ciaone', 'bella ', 'hey']
 
+    _INSULT = ['coglione', 'ricchione', 'stronzo', 'frocio', 'merda', 'cesso',
+               'gay', 'idiota', 'imbecille', 'pure tu']
+
     def __init__(self, botId, botName, client, socket_delay=1):
         self.botId = botId
         self.botName = botName
@@ -39,6 +42,22 @@ class MessageHandler(object):
         if self.botName in messageText.split():
             return 'name'
 
+    def _decorateMessage(self, userId, text, mention=False, front=True,
+                         toChannel=False):
+        if toChannel:
+            return '{0} {1}'.format(text, 'a tutti')
+
+        if mention:
+            userName = self.getMention(userId)
+        else:
+            # TODO get only the userName
+            userName = self.getMention(userId)
+
+        if front:
+            return '{0} {1}'.format(userName, text)
+        else:
+            return '{0} {1}'.format(text, userName)
+
     def _postMessage(self, messageText, channel):
         self.client.api_call(
             'chat.postMessage', channel=channel,
@@ -50,26 +69,43 @@ class MessageHandler(object):
                 return True
 
     def sayHi(self, user, channel, mention=False):
+        """In private channel: say Hi + username
+        In public: say Hi + username if mention else Hi everybody"""
         # TODO hack
         if channel.startswith('D'):
-            userName = self.getMention(user)
-            messageText = '{0} {1}'.format(
-                random.choice(self._HI), userName)
-            self._postMessage(messageText=messageText, channel=channel)
-
+            mention = False
+            messageText = self._decorateMessage(
+                user, random.choice(self._HI), mention=mention, front=False)
         elif mention:
-            userMention = self.getMention(user)
-            messageText = '{0} {1}'.format(random.choice(self._HI), userMention)
-            self._postMessage(messageText=messageText, channel=channel)
-
+            messageText = self._decorateMessage(
+                user, random.choice(self._HI), mention=mention, front=False)
         else:
-            # TODO generalizzare a tutti
-            messageText = '{0} {1}'.format(random.choice(self._HI), 'a tutti')
-            self._postMessage(messageText=messageText, channel=channel)
+            messageText = self._decorateMessage(
+                user, random.choice(self._HI), toChannel=True)
+
+        self._postMessage(messageText=messageText, channel=channel)
+
+    def isInsult(self, message):
+        for token in message.split(' '):
+            if token in self._INSULT:
+                return True
+
+    def sayInsult(self, user, channel, mention=False):
+        if channel.startswith('D') or not mention:
+            mention = False
+            messageText = self._decorateMessage(
+                user, random.choice(self._INSULT), mention=mention)
+        elif mention:
+            messageText = self._decorateMessage(
+                user, random.choice(self._INSULT), mention=mention)
+
+        self._postMessage(messageText=messageText, channel=channel)
 
     def handleMessage(self, message, user, channel, mention):
         if self.isHi(message=message):
             return self.sayHi(user, channel, mention)
+        elif self.isInsult(message=message):
+            return self.sayInsult(user, channel, mention)
 
     def run(self):
         if not self.client.rtm_connect():
